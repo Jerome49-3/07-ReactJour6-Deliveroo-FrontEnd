af5a0e358cb64873dfc6fe3fafb79b415d04157a
jerome <151376824+Jerome49-3@users.noreply.github.com>
2024-06-11 10:30:22 +0200

    aside almost done

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -18,52 +18,52 @@ const Main = ({ data, classMain, classTxt }) => {
           <Image src={rests.picture} classImg="imgBanner" />
         </section>
         <section className='bouffe'>
-          <div className="boxPlats wrapper">
-            {categ.map((repas, index) => {
-              // console.log('repas:', repas);
-              if (repas.length !== 0) {
-                return (
-                  <section>
-                    <Title title={repas.name} classTxt='titleCateg' />
-                    <div className="boxCards">
-                      {repas.meals.map((menus, key = menus.id) => {
-                        console.log('menus:', menus);
-                        return (
-                          <>
-                            <article key={key} className='card'>
-                              <div className="left">
-                                <Title title={menus.title} classTxt="titleCard" />
-                                <p></p>
-                                <div className="footerCard">
-                                  <p>{menus.description}</p>
-                                  <div>
-                                    <p>{menus.price}</p>
-                                    {menus.populaire === true ? <div>star</div> : <div></div>}
+          <section className='wrapper'>
+            <div className="boxPlats">
+              {categ.map((repas, index) => {
+                // console.log('repas:', repas);
+                if (repas.length !== 0) {
+                  return (
+                    <section>
+                      <Title title={repas.name} classTxt='titleCateg' />
+                      <div className="boxCards">
+                        {repas.meals.map((menus, key = menus.id) => {
+                          console.log('menus:', menus);
+                          return (
+                            <>
+                              <article key={key} className='card'>
+                                <div className="left">
+                                  <Title title={menus.title} classTxt="titleCard" />
+                                  <p></p>
+                                  <div className="footerCard">
+                                    <p>{menus.description}</p>
+                                    <div>
+                                      <p>{menus.price}</p>
+                                      {menus.populaire === true ? <div>star</div> : <div></div>}
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                              <div className="right">
-                                {menus.picture ? <Image src={menus.picture} /> : <div></div>}
-                              </div>
-                            </article>
-                          </>
-                        )
-                      })}
-                    </div>
-                  </section>
-                )
-              }
-            })}
-          </div>
-          <aside>
-            <div className='asideTop'>
-              <article className='panier'>
-                <h3>Valider mon panier</h3>
-                <div>
-                  <p>votre panier est vide</p>
-                </div>
-              </article>
+                                <div className="right">
+                                  {menus.picture ? <Image src={menus.picture} /> : <div></div>}
+                                </div>
+                              </article>
+                            </>
+                          )
+                        })}
+                      </div>
+                    </section>
+                  )
+                }
+              })}
             </div>
+          </section>
+          <aside>
+            <article className='panier'>
+              <h3>Valider mon panier</h3>
+              <div>
+                <p>votre panier est vide</p>
+              </div>
+            </article>
           </aside>
         </section>
       </main>
